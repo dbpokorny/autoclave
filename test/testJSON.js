ee6ae8852e7270cbda54ed5205efd8a80b5b3e46
David Brooks Pokorny <dbpokorny@gmail.com>
2015-11-20 20:17:16 -0800

    .

--- test/testJSON.js
+++ test/testJSON.js
@@ -4,6 +4,8 @@ var obj = {
     "a" : [1,2,3],
     "bc" : [[],[]],
     "def" : { "g" : "hi" },
+    "test" : "push",
+    "push" : "test"
 };
 console.log(JSON.stringify(obj));
 console.log(JSON.stringify(JSON.parse(JSON.stringify(obj))));
